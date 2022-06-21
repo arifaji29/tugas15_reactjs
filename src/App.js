d66e7dce5e7be83010b281135d945dafa7d271ed
Arif <arifajif@gmail.com>
2022-06-22 00:44:45 +0700

    tugas15

--- src/App.js
+++ src/App.js
@@ -1,24 +1,229 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Grid, Form, Feed, Button, Header, Comment, Divider, Segment, Input, Search, Container, Icon, Dimmer, Label, Loader, Image, Placeholder, List, Table, Menu } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Container textAlign='center'>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colSpan='3'>
+                <Grid.Column>
+                  <Container textAlign='right'>
+                    <Search placeholder='Search...' />
+                  </Container>
+                </Grid.Column>
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell textAlign="center">Nama Document</Table.HeaderCell>
+              <Table.HeaderCell textAlign="center">Jenis File</Table.HeaderCell>
+
+            </Table.Row>
+          </Table.Header>
+
+
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Label ribbon>Panduan Belajar Javascript</Label>
+              </Table.Cell>
+              <Table.Cell>pdf</Table.Cell>
+
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Panduan Belajar CSS</Table.Cell>
+              <Table.Cell>pdf</Table.Cell>
+
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Panduan Belajar React JS</Table.Cell>
+              <Table.Cell>pdf</Table.Cell>
+
+            </Table.Row>
+          </Table.Body>
+
+          <Table.Footer>
+            <Table.Row>
+              <Table.HeaderCell colSpan='3'>
+                <Menu floated='right' pagination>
+                  <Menu.Item as='a' icon>
+                    <Icon name='chevron left' />
+                  </Menu.Item>
+                  <Menu.Item as='a'>1</Menu.Item>
+                  <Menu.Item as='a'>2</Menu.Item>
+                  <Menu.Item as='a'>3</Menu.Item>
+                  <Menu.Item as='a'>4</Menu.Item>
+                  <Menu.Item as='a' icon>
+                    <Icon name='chevron right' />
+                  </Menu.Item>
+                </Menu>
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Footer>
+        </Table>
+      </Container>
+      {/* </Grid.Column>
+      </Grid> */}
+
+
+      <br>
+      </br>
+      <Grid>
+
+
+
+
+        <Grid.Column floated='left' width={4} style={{marginLeft:'100px'}}>
+          <Container>
+            <Comment.Group threaded>
+              <Header as='h3' dividing>
+                Comments
+              </Header>
+              <Comment>
+                <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+                <Comment.Content>
+                  <Comment.Author as='a'>Satria</Comment.Author>
+                  <Comment.Metadata>
+                    <span>Today at 5:42PM</span>
+                  </Comment.Metadata>
+                  <Comment.Text>Hallo</Comment.Text>
+                  <Comment.Actions>
+                    <a>Reply</a>
+                  </Comment.Actions>
+                </Comment.Content>
+              </Comment>
+
+              <Comment>
+                <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                <Comment.Content>
+                  <Comment.Author as='a'>Danang</Comment.Author>
+                  <Comment.Metadata>
+                    <span>Yesterday at 12:30AM</span>
+                  </Comment.Metadata>
+                  <Comment.Text>
+                    <p>Kerjakan sesuatu semampu kamu</p>
+                  </Comment.Text>
+                  <Comment.Actions>
+                    <a>Reply</a>
+                  </Comment.Actions>
+                </Comment.Content>
+
+                <Comment.Group>
+                  <Comment>
+                    <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+                    <Comment.Content>
+                      <Comment.Author as='a'>Ricky</Comment.Author>
+                      <Comment.Metadata>
+                        <span>Just now</span>
+                      </Comment.Metadata>
+                      <Comment.Text>Sekarang kamu dimana danang?</Comment.Text>
+                      <Comment.Actions>
+                        <a>Reply</a>
+                      </Comment.Actions>
+                    </Comment.Content>
+                  </Comment>
+                </Comment.Group>
+              </Comment>
+
+              <Comment>
+                <Comment.Avatar as='a' src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+                <Comment.Content>
+                  <Comment.Author as='a'>Joe</Comment.Author>
+                  <Comment.Metadata>
+                    <span>5 days ago</span>
+                  </Comment.Metadata>
+                  <Comment.Text>Terima Kasih atas nasihatnya</Comment.Text>
+                  <Comment.Actions>
+                    <a>Reply</a>
+                  </Comment.Actions>
+                </Comment.Content>
+              </Comment>
+
+              <Form reply>
+                <Form.TextArea />
+                <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+              </Form>
+            </Comment.Group>
+          </Container>
+        </Grid.Column >
+
+
+
+        <Grid.Column floated='right' width={5}>
+          <Container textAlign='right'>
+            <Feed>
+              <Feed.Event>
+                <Feed.Label>
+                  <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                </Feed.Label>
+                <Feed.Content>
+                  <Feed.Summary>
+                    <Feed.User>Anton</Feed.User> added you as a friend
+                    <Feed.Date>3 Hour Ago</Feed.Date>
+                  </Feed.Summary>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name='like' />4 Likes
+                    </Feed.Like>
+                  </Feed.Meta>
+                </Feed.Content>
+              </Feed.Event>
+
+
+              <Feed.Event>
+                <Feed.Label>
+                  <Icon name='pencil' />
+                </Feed.Label>
+                <Feed.Content>
+                  <Feed.Date>Today</Feed.Date>
+                  <Feed.Summary>
+                    You submitted a new post to the page
+                  </Feed.Summary>
+                  <Feed.Extra text>
+                    Saya senang belajar bahasa pemrrograman
+                  </Feed.Extra>
+                </Feed.Content>
+              </Feed.Event>
+
+
+              <Feed.Event>
+                <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
+                <Feed.Content>
+                  <Feed.Date>4 days ago</Feed.Date>
+                  <Feed.Summary>
+                    <a>David</a> added <a>2 new images</a>
+                  </Feed.Summary>
+                  <Feed.Extra images>
+                    <a>
+                      <img src='https://cdn-images-1.medium.com/max/1600/1*6kK9j74vyOmXYm1gN6ARhQ.png' />
+                    </a>
+                    <a>
+                      <img src='https://s3-ap-southeast-1.amazonaws.com/niomic/img-v1/c_login_logo.png' />
+                    </a>
+                  </Feed.Extra>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name='like' />2 Like
+                    </Feed.Like>
+                  </Feed.Meta>
+                </Feed.Content>
+              </Feed.Event>
+            </Feed>
+          </Container>
+        </Grid.Column>
+
+
+      </Grid>
+
+    </>
   );
 }
 
